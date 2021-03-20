2762f700f723dbbf70ef8def23cafb51344ad9e2
FelipeCamilo <felipelindinhobat@gmail.com>
2021-03-20 02:15:40 +0000

    Aula 01 - Primeiro componente

---
+++ fundamentos01/src/components/basico/OlaMundo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => {
+//function
+    const informacao = 'Primeiro componente JSX'
+
+    return(
+        <>
+            <h2>Componente OlaMundo</h2>
+            <p>{ informacao }</p>
+        </>
+    )
+}
